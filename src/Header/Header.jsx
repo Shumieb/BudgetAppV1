66d193da96e11b6da87a118f968225777a747a0d
Shumieb <shumiebc@gmail.com>
2024-04-21 13:26:44 +0100

    Header styles added

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,14 @@
 import { NavLink } from "react-router-dom"
+import styles from "./Header.module.css"
 
 function Header() {
     return (
         <header>
-            <p>My Budget App</p>
+            <h1>My Budget App</h1>
             <nav>
                 <ul>
-                    <li><NavLink to="/">Home</NavLink> </li>
-                    <li><NavLink to="/about">About</NavLink> </li>
+                    <li><NavLink to="/">Home</NavLink> </li>|
+                    <li><NavLink to="/about">About</NavLink> </li>|
                     <li><NavLink to="/contact">Contact</NavLink> </li>
                 </ul>
             </nav>
